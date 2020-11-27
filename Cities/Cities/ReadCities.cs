34bd4713fbef3e789fc571b663aa3b28c0082fd1
lusparon332 <lusparon222@gmail.com>
2020-11-27 20:36:19 +0300

    add func to read cities

---
+++ Cities/Cities/ReadCities.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Cities
+{
+    public class ReadCities
+    {
+        /// <summary>
+        /// Считывает из файла названия городов
+        /// </summary>
+        /// <param name="path">путь к файлу</param>
+        /// <returns>Массив строк, где каждая строка - название города</returns>
+        public static string[] ReadCitiesFunc(string path)
+        {
+            return File.ReadAllLines(path);
+        }
+    }
+}
