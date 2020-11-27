0a8f8fe3ab5667c03a97316b59dec4d181cef10f
your-panacea <lotheb@mail.ru>
2020-11-27 20:56:56 +0300

    add Class1 f

---
+++ Cities/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cities
+{
+    public class task3
+    {
+        string citiesList;
+        public bool FindCities(string a)
+        {
+            return citiesList.Contains(a);
+        }
+    }
+}
